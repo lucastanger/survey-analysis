26415badc2c6ea040c0b060e2ab9c3c554eff192
Luca Stanger <39199539+lucastanger@users.noreply.github.com>
2020-12-11 12:48:29 +0100

    :recycle: REFACTOR: apply tidyverse style

--- load_data.R
+++ load_data.R
@@ -3,6 +3,6 @@ if(!require(tidyverse)) {
   require(tidyverse)
 }
 
-so_20 <- read.csv(file = 'data/survey_results_public.csv')
+so_20 <- read_csv(file = 'data/survey_results_public.csv')
 
-so_20_schema <- read.csv('data/survey_results_schema.csv')
+so_20_schema <- read_csv('data/survey_results_schema.csv')
